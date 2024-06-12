7feb7f1164dc723467c00974e13b9b9dc615caac
Adebart <pelumiayuba3@gmail.com>
2024-06-12 09:40:22 +0100

    first

---
+++ main.js
@@ -0,0 +1,28 @@
+function click1(){
+document.getElementById('a1').style.display= 'block'
+document.getElementById('a2').style.display= 'none'
+document.getElementById('a3').style.display= 'none'
+document.getElementById('a4').style.display= 'none'
+}
+
+function click2(){
+    document.getElementById('a2').style.display= 'block'
+document.getElementById('a1').style.display= 'none'
+document.getElementById('a3').style.display= 'none'
+document.getElementById('a4').style.display= 'none'
+
+}
+function click3(){
+    document.getElementById('a3').style.display= 'block'
+    document.getElementById('a1').style.display= 'none'
+document.getElementById('a2').style.display= 'none'
+document.getElementById('a4').style.display= 'none'
+
+}
+function click9(){
+    document.getElementById('a4').style.display= 'block'
+    document.getElementById('a1').style.display= 'none'
+document.getElementById('a2').style.display= 'none'
+document.getElementById('a3').style.display= 'none'
+    // alert('ggggg')
+}
